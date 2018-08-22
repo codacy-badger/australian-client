d673d7c165693aae9e90888d8cffce5de3fbdc14
Alexandre-T <alexandre.tranchant@gmail.com>
2018-08-22 09:34:26 +0200

    Test updated.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import {translate} from "react-i18next";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(translate("translations")(App), div);
   ReactDOM.unmountComponentAtNode(div);
 });
