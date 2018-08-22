4f6c79aa7ba98ec80d06d40c2b11f2367a24bb24
Alexandre-T <alexandre.tranchant@gmail.com>
2018-08-22 11:16:46 +0200

    Login form component designed.

--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -1,10 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { translate } from "react-i18next";
+import {Col, Form, FormGroup, Input, Label} from "reactstrap";
 
 class LoginForm extends Component {
   render() {
-    return <div>Login form</div>;
+    const {t} = this.props;
+
+    return (
+      <Form {...this.props}>
+        <FormGroup row>
+          <Label for="loginEmail" sm={4}>{t('form.login.email')}</Label>
+          <Col sm={8}>
+            <Input type="email" name="email" id="loginEmail" placeholder="john.doe@example.org" required/>
+          </Col>
+        </FormGroup>
+        <FormGroup row>
+          <Label for="loginPassword" sm={4}>{t('form.login.password')}</Label>
+          <Col sm={8}>
+            <Input type="password" name="password" id="loginPassword" placeholder={t('form.login.password-placeholder')} required/>
+          </Col>
+        </FormGroup>
+      </Form>
+    );
   }
 }
 
