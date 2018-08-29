1ac45f5cb6ff90018a573f6004b3b4402fe93c42
Alexandre-T <alexandre.tranchant@gmail.com>
2018-08-29 11:58:25 +0200

    Converted to full react component.

--- src/components/common/alert/StatusAlert.js
+++ src/components/common/alert/StatusAlert.js
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Alert } from "reactstrap";
 import { translate, Trans } from "react-i18next";
 
-const StatusAlert = ({ code, error = {}, isError, isPending, isSuccess, success = {}, t }) => {
-  const color = isError ? "danger" : isSuccess ? "success" : "info";
-  const key = isError ? "error." + error.code : isSuccess ? "message." + code + "." + success.code : null;
-  const message = isError ? error.message : isSuccess ? success.message : t("help." + code);
+class StatusAlert extends Component {
+  render() {
+    const { code, error, isError, isSuccess, success, t } = this.props;
 
-  return (
-    <Alert color={color} className="text-center">
-      {null !== key && <Trans i18nKey={key}>{message}</Trans>}
-      {null === key && message}
-    </Alert>
-  );
-};
+    //Calculation
+    const color = isError ? "danger" : isSuccess ? "success" : "info";
+    const key = isError ? "error." + error.code : isSuccess ? "message." + code + "." + success.code : null;
+    const message = isError ? error.message : isSuccess ? success.message : t("help." + code);
+
+    return (
+      <Alert color={color} className="text-center">
+        {null !== key && <Trans i18nKey={key}>{message}</Trans>}
+        {null === key && message}
+      </Alert>
+    );
+  }
+}
 
 StatusAlert.propTypes = {
+  code: PropTypes.string.isRequired,
   error: PropTypes.object.isRequired,
   isError: PropTypes.bool.isRequired,
   isSuccess: PropTypes.bool.isRequired,
