8e61927e86d528ba1c4b8d9f70be968362e478b2
Alexandre-T <alexandre.tranchant@gmail.com>
2018-09-28 17:28:51 +0200

    Login modal simplification.

--- src/components/page/LoginPage.js
+++ src/components/page/LoginPage.js
@@ -1,34 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
+import Header from "../common/Header";
+import LoginModal from "../modal/LoginModal";
+import Meta from "../common/Meta";
 import { Container, UncontrolledAlert } from "reactstrap";
 import { translate } from "react-i18next";
-import LoginModal from "../modal/LoginModal";
-import Header from "../common/Header";
-import Meta from "../common/Meta";
 
-class LoginPage extends Component {
-  toggleLoginModal = () => {
-    this.loginModal.toggle();
-  };
-
-  componentDidMount() {
-    this.toggleLoginModal();
-  }
-
-  render() {
-    const { t } = this.props;
-    return (
-      <div>
-        <Meta code="login" />
-        <Header />
-        <Container className="mt-3">
-          <UncontrolledAlert color="warning">{t("message.please-login")}</UncontrolledAlert>
-          <LoginModal onRef={(ref) => (this.loginModal = ref)} warning={true} />
-        </Container>
-      </div>
-    );
-  }
-}
+const LoginPage = ({ t }) => {
+  return (
+    <div>
+      <Meta code="login" />
+      <Header />
+      <Container className="mt-3">
+        <UncontrolledAlert color="warning">{t("message.please-login")}</UncontrolledAlert>
+        <LoginModal isOpen warning />
+      </Container>
+    </div>
+  );
+};
 
 LoginPage.propTypes = {
   t: PropTypes.func.isRequired
