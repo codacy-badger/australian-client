df783e95fc103dfb94e45fba90fae18edd49b6a4
Alexandre-T <alexandre.tranchant@gmail.com>
2018-09-28 10:52:35 +0200

    form without antipattern done.

--- src/components/container/profile/AccountContainer.js
+++ src/components/container/profile/AccountContainer.js
@@ -1,11 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import PasswordFormGroup from "../../formgroup/PasswordFormGroup";
-import {Button} from "reactstrap";
+import { Button, Form } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { translate } from "react-i18next";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { createForm, formShape } from "rc-form";
 
 library.add(faTrashAlt);
 
@@ -17,6 +18,8 @@ class AccountContainer extends Component {
     };
 
     this.onChange = this.onChange.bind(this);
+    this.submit = this.submit.bind(this);
+    this.validateFields = this.validateFields.bind(this);
   }
 
   onChange(e) {
@@ -25,29 +28,45 @@ class AccountContainer extends Component {
     });
   }
 
-  render () {
-    const {password} = this.state;
-    const {t} = this.props;
+  validateFields(e) {
+    e.preventDefault();
+
+    this.props.form.validateFields((error) => {
+      if (!error) {
+        this.onSubmit(e);
+      }
+    });
+  }
+
+  submit(e) {
+    //FIXME redux calls
+    console.log("do the job");
+  }
+
+  render() {
+    const { password } = this.state;
+    const { form, t } = this.props;
 
     return (
-      <div className="text-danger">
+      <Form onSubmit={this.validateFields} className="text-danger">
         <h2>{t("title.account")}</h2>
         <p>{t("form.account.description")}</p>
-        <PasswordFormGroup onChange={this.onChange} value={password}/>
+        <PasswordFormGroup onChange={this.onChange} value={password} form={form} />
         <div className="text-right">
-          <Button color="danger">
-            <FontAwesomeIcon icon="trash-alt" className={"mr-1"}/>
+          <Button color="danger" onClick={this.validateFields}>
+            <FontAwesomeIcon icon="trash-alt" className={"mr-1"} />
             {t("form.account.submit")}
           </Button>
         </div>
-      </div>
+      </Form>
     );
   }
 }
 
 // The propTypes.
 AccountContainer.propTypes = {
+  form: formShape,
   t: PropTypes.func.isRequired
 };
 
-export default translate("translations")(AccountContainer);
+export default translate("translations")(createForm()(AccountContainer));
--- src/components/formgroup/PasswordFormGroup.js
+++ src/components/formgroup/PasswordFormGroup.js
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 import HelpBlockErrors from "../common/help/HelpBlockErrors";
 import InputGroupIcon from "../common/input/InputGroupIcon";
 import { Col, FormGroup, Input, InputGroup, Label } from "reactstrap";
-import { createForm } from "rc-form";
 import { faKey } from "@fortawesome/free-solid-svg-icons";
+import { formShape } from "rc-form";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { translate } from "react-i18next";
 
@@ -43,8 +43,11 @@ const PasswordFormGroup = ({ children, form, onChange, t, value }) => {
 };
 
 PasswordFormGroup.propTypes = {
+  children: PropTypes.element,
+  form: formShape,
   onChange: PropTypes.func.isRequired,
+  t: PropTypes.func.isRequired,
   value: PropTypes.string.isRequired
 };
 
-export default translate("translations")(createForm()(PasswordFormGroup));
+export default translate("translations")(PasswordFormGroup);
