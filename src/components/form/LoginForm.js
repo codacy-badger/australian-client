15739e10ba79b9f1e969eb33988a228655635dc2
Alexandre-T <alexandre.tranchant@gmail.com>
2018-09-29 18:50:38 +0200

    Transform to a stateless React component.

--- src/components/form/LoginForm.js
+++ src/components/form/LoginForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import EmailFormGroup from "../formgroup/EmailFormGroup";
 import PasswordFormGroup from "../formgroup/PasswordFormGroup";
@@ -9,41 +9,38 @@ import { Link } from "react-router-dom";
 import { formShape } from "rc-form";
 import { translate } from "react-i18next";
 
-//TODO Convert into stateless react component.
-class LoginForm extends Component {
-  render() {
-    const { email, form, isPending, onChange, onSubmit, password, rememberMe, submitRender, t } = this.props;
+const LoginForm = (props) => {
+  const { email, form, isPending, onChange, onSubmit, password, rememberMe, submitRender, t } = props;
 
-    return (
-      <Form onSubmit={onSubmit}>
-        <EmailFormGroup form={form} onChange={onChange} value={email} />
-        <PasswordFormGroup form={form} onChange={onChange} value={password}>
-          <FormText color="muted">
-            <FontAwesomeIcon fixedWidth icon="info-circle" className="mr-1 text-info" />
-            <Link to="/forgot-your-password" title={t("link.forgot-your-password-title")}>
-              {t("link.forgot-your-password")}
-            </Link>
-          </FormText>
-        </PasswordFormGroup>
-        <FormGroup check>
-          <Col sm={{ size: 8, offset: 4 }}>
-            <Label check>
-              <Input type="checkbox" name="rememberMe" id="rememberMe" checked={rememberMe} onChange={onChange} />
-              {t("form.login.rememberMe")}
-            </Label>
-          </Col>
-        </FormGroup>
-        {submitRender && <Submit icon="sign-in-alt" name="login" isPending={isPending} onClick={onSubmit} />}
-      </Form>
-    );
-  }
-}
+  return (
+    <Form onSubmit={onSubmit}>
+      <EmailFormGroup form={form} onChange={onChange} value={email} />
+      <PasswordFormGroup form={form} onChange={onChange} value={password}>
+        <FormText color="muted">
+          <FontAwesomeIcon fixedWidth icon="info-circle" className="mr-1 text-info" />
+          <Link to="/forgot-your-password" title={t("link.forgot-your-password-title")}>
+            {t("link.forgot-your-password")}
+          </Link>
+        </FormText>
+      </PasswordFormGroup>
+      <FormGroup check>
+        <Col sm={{ size: 8, offset: 4 }}>
+          <Label check>
+            <Input type="checkbox" name="rememberMe" id="rememberMe" checked={rememberMe} onChange={onChange} />
+            {t("form.login.rememberMe")}
+          </Label>
+        </Col>
+      </FormGroup>
+      {submitRender && <Submit icon="sign-in-alt" name="login" isPending={isPending} onClick={onSubmit} />}
+    </Form>
+  );
+};
 
+// The propTypes.
 LoginForm.defaultProps = {
   submitRender: false
 };
 
-// The propTypes.
 LoginForm.propTypes = {
   email: PropTypes.string.isRequired,
   form: formShape,
