36c14fde6577b4a7dd96fc1620274211f630d5f8
Alexandre-T <alexandre.tranchant@gmail.com>
2018-10-01 12:05:36 +0200

    Login form disabled when submitting request.

--- src/components/form/PasswordForm.js
+++ src/components/form/PasswordForm.js
@@ -10,7 +10,6 @@ import { formShape } from "rc-form";
 
 library.add(faTrashAlt);
 
-//TODO add loading props
 const PasswordForm = (props) => {
   const { confirmation, form, isPending, onChange, onSubmit, oldPassword, newPassword} = props;
 
