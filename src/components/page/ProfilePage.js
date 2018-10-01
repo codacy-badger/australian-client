30ee1a49515a5185580a61bc9ecb7f1f58053ce9
Alexandre-T <alexandre.tranchant@gmail.com>
2018-10-01 16:18:57 +0200

    Convert to stateless react component

--- src/components/page/ProfilePage.js
+++ src/components/page/ProfilePage.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import AccountContainer from "../container/profile/AccountContainer";
 import AddressForm from "../form/AddressForm";
@@ -12,57 +12,51 @@ import { Col, Container, ListGroup, ListGroupItem, Row } from "reactstrap";
 import { NavLink, Route, Switch } from "react-router-dom";
 import { translate } from "react-i18next";
 
-//TODO Convert to stateless react component.
-class ProfilePage extends Component {
-  render() {
-    const { t } = this.props;
-
-    return (
-      <div>
-        <Meta code="profile" />
-        <Header />
-        <Container className="mt-3 text-justify">
-          <h1>{t("title.profile")}</h1>
-          <Row>
-            <Col sm={4} lg={3}>
-              <ListGroup>
-                <ListGroupItem tag={NavLink} to="/profile/general">
-                  {t("profile.tab.general")}
-                </ListGroupItem>
-                <ListGroupItem tag={NavLink} to="/profile/address">
-                  {t("profile.tab.address")}
-                </ListGroupItem>
-                <ListGroupItem tag={NavLink} to="/profile/mail">
-                  {t("profile.tab.mail")}
-                </ListGroupItem>
-                <ListGroupItem tag={NavLink} to="/profile/password">
-                  {t("profile.tab.password")}
-                </ListGroupItem>
-                <ListGroupItem tag={NavLink} to="/profile/account">
-                  {t("profile.tab.account")}
-                </ListGroupItem>
-                <ListGroupItem tag={NavLink} to="/profile/cookies">
-                  {t("profile.tab.cookies")}
-                </ListGroupItem>
-              </ListGroup>
-            </Col>
-            <Col>
-              <Switch>
-                <Route exact path="/profile/general" component={ProfileContainer} />
-                <Route exact path="/profile/account" component={AccountContainer} />
-                <Route exact path="/profile/address" component={AddressForm} />
-                <Route exact path="/profile/cookies" component={CookieContainer} />
-                <Route exact path="/profile/password" component={PasswordContainer} />
-                {/* TODO replace by a component non displaying header */}
-                <Route path="/profile/*" component={NotFoundJumbotron} />
-              </Switch>
-            </Col>
-          </Row>
-        </Container>
-      </div>
-    );
-  }
-}
+const ProfilePage = ({ t }) => {
+  return (
+    <div>
+      <Meta code="profile" />
+      <Header />
+      <Container className="mt-3 text-justify">
+        <h1>{t("title.profile")}</h1>
+        <Row>
+          <Col sm={4} lg={3}>
+            <ListGroup>
+              <ListGroupItem tag={NavLink} to="/profile/general">
+                {t("profile.tab.general")}
+              </ListGroupItem>
+              <ListGroupItem tag={NavLink} to="/profile/address">
+                {t("profile.tab.address")}
+              </ListGroupItem>
+              <ListGroupItem tag={NavLink} to="/profile/mail">
+                {t("profile.tab.mail")}
+              </ListGroupItem>
+              <ListGroupItem tag={NavLink} to="/profile/password">
+                {t("profile.tab.password")}
+              </ListGroupItem>
+              <ListGroupItem tag={NavLink} to="/profile/account">
+                {t("profile.tab.account")}
+              </ListGroupItem>
+              <ListGroupItem tag={NavLink} to="/profile/cookies">
+                {t("profile.tab.cookies")}
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col>
+            <Switch>
+              <Route exact path="/profile/general" component={ProfileContainer} />
+              <Route exact path="/profile/account" component={AccountContainer} />
+              <Route exact path="/profile/address" component={AddressForm} />
+              <Route exact path="/profile/cookies" component={CookieContainer} />
+              <Route exact path="/profile/password" component={PasswordContainer} />
+              <Route path="/profile/*" component={NotFoundJumbotron} />
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
 // The propTypes.
 ProfilePage.propTypes = {
