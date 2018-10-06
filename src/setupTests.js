bdf0b3674b81017aa5dbb2f9694c28c689e720d7
Alexandre-T <alexandre.tranchant@gmail.com>
2018-10-06 15:56:03 +0200

    Strings must use doublequote. (quotes)

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,7 @@ import { LocalStorage } from "node-localstorage";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-global.localStorage = new LocalStorage('./scratch/localStorage');
-global.sessionStorage = new LocalStorage('./scratch/sessionStorage');
+global.localStorage = new LocalStorage("./scratch/localStorage");
+global.sessionStorage = new LocalStorage("./scratch/sessionStorage");
 
 configure({ adapter: new Adapter() });
