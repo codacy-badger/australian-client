ca020e8d81656cc75a8cc051bb7d06e5b284e7e5
Alexandre-T <alexandre.tranchant@gmail.com>
2018-10-13 18:13:22 +0200

    Selecting store is now fixed.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import configureStore from "./store/configureStore.prod";
+import configureStore from "./store/configureStore";
 
 import "./index.css";
 import App from "./App";
