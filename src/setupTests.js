616994fbfffe4f73eefc04c123da3988740c1bc3
Alexandre-T <alexandre.tranchant@gmail.com>
2018-10-22 15:09:33 +0200

    Removing not-used tests.

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,23 +2,8 @@ import { LocalStorage } from "node-localstorage";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-const jsdom = require('jsdom').jsdom;
-const exposedProperties = ['window', 'navigator', 'document'];
-
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    exposedProperties.push(property);
-    global[property] = document.defaultView[property];
-  }
-});
-
-global.navigator = {
-  userAgent: 'node.js'
-};
-
 global.localStorage = new LocalStorage("./scratch/localStorage");
 global.sessionStorage = new LocalStorage("./scratch/sessionStorage");
 
+//Configuring enzyme
 configure({ adapter: new Adapter() });
